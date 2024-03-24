a69fce40c755b7c778bee6c7f7de154419497c94
joelsteven <joelofelectronics@gmail.com>
2024-03-24 19:48:49 +0300

    Add OtpController and AuthMiddleware to routes***

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import UsersController from "../controllers/UsersController";
+import OtpController from "../controllers/OtpController";
+import AuthMiddleware from "../middleware/AuthMiddleware";
 
 const router = express.Router();
 const prefix = "/api/v1";
@@ -7,7 +9,14 @@ const prefix = "/api/v1";
 // auth routes
 router.post(`${prefix}/auth/register`, UsersController.signUp);
 router.post(`${prefix}/auth/login`, UsersController.logIn);
-router.get(`${prefix}/auth/me`, UsersController.getUser);
+router.get(
+  `${prefix}/auth/me`,
+  AuthMiddleware.authenticate,
+  UsersController.getUser
+);
 router.get(`${prefix}/auth/logout`, UsersController.logOut);
+
+// send otp route
+router.post(`${prefix}/auth/otp`, OtpController.sendOtp);
 
 export default router;
