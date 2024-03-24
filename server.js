406a7323128dbe6dd6509ae05cb9ab30ae6d611f
joelsteven <joelofelectronics@gmail.com>
2024-03-24 19:49:46 +0300

    Add cors middleware to enable cross-origin resource sharing

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import router from "./routes";
 import dbClient from "./utils/db";
 import redisClient from "./utils/redis";
@@ -9,6 +10,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cors());
 app.use("/", router);
 
 app.listen(port, () => {
